42840fb66d1cefc77de5684d4f869a10b3be8d6f
Uwe <uwe@sci-d-vis.com>
2026-04-30 18:52:47 +0200

    v0.10.0 (docitem: v0.6.1, waterlint: 0.9.0, JSON-schema: 0.1.0)

--- do_mypy_simple.sh
+++ do_mypy_simple.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
-PATH_MYPY_INI=$(realpath $(dirname $0)/mypy.ini)
+set -euo pipefail
 
-mypy --config-file ${PATH_MYPY_INI} -p sdv.doc.waterloo
+SCRIPT_DIR=$(realpath $(dirname $0))
+PATH_MYPY_INI=$(realpath $(dirname $0)/mypy.ini)
+PATH_SRC_DIR=$(realpath $(dirname $0)/src/sdv/doc/waterloo)
+PATH_CHK_OUT=$(realpath $(dirname $0)/doc/source/_static/type_checking_report.txt)
+PATH_EXC_OUT=$(realpath $(dirname $0)/doc/source/_static/type_checking_exceptions.txt)
+
+MYPYPATH="${SCRIPT_DIR}/src" mypy --config-file "${PATH_MYPY_INI}" \
+	--namespace-packages \
+	--explicit-package-bases \
+	"${PATH_SRC_DIR}" \
+	> "${PATH_CHK_OUT}"
+
+grep -nE '#[[:space:]]*(type: ignore(\[[^]]+\])?|pragma: no cover.*)$' "${PATH_SRC_DIR}"/*.py \
+| awk -F: 'match($0, /#[[:space:]]*(type: ignore(\[[^]]+\])?|pragma: no cover.*)$/, m) { n = split($1, p, "/"); printf "%s:%s %s\n", p[n], $2, m[0] }' \
+> "${PATH_EXC_OUT}"
